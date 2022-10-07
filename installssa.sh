942d620e6398c57917039719be0552a7c9d2ab00
cheechi <97264902+professorcheechi@users.noreply.github.com>
2022-10-06 21:28:30 -0400

    Update installssa.sh

--- installssa.sh
+++ installssa.sh
@@ -5,33 +5,63 @@
 # https://askubuntu.com/questions/1295102/how-do-i-add-repo-gpg-keys-as-apt-key-is-deprecated
 # https://askubuntu.com/questions/1286545/what-commands-exactly-should-replace-the-deprecated-apt-key
 
-url=http://downloads.linux.hpe.com/SDR/
+usage()
+{
+  printf "This script will pull the pub keys from HP, create a keyring & trusted repo, and install ssacli \n"
+  printf "for managing HP Smart Array storage controllers. It is intended to be run with sudo or as root. \n\n"
+}
 
 
-
-for key in hpPublicKey1024.pub hpPublicKey2048.pub hpPublicKey2048_key1.pub hpePublicKey2048_key1.pub ; do
-
-  wget -q -O - "$url$key" |
-#  wget "$url$key" |
-    sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/hp-mcp.gpg --import -
-
-done
-
-sudo chmod 644 /etc/apt/trusted.gpg.d/hp-mcp.gpg
-
-if [ ! -f /etc/apt/sources.list.d/hp-mcp.list ]; then
-
-addrepo=$(sudo echo "deb [ signed-by=/etc/apt/trusted.gpg.d/hp-mcp.gpg ] http://downloads.linux.hpe.com/SDR/repo/mcp stretch/>
-
-  if [ $(echo "$addrepo" | grep -i denied) ]; then
-    sudo touch /etc/apt/sources.list.d/hp-mcp.list
-    printf "\nPlease do: \n sudo nano /etc/apt/sources.list.d/hp-mcp.list and paste the line \n"
-    printf " >> deb [signed by=/etc/apt/trusted.gpg.d/hp-mcp.gpg ] http://downloads.linux.hpe.com/SDR/repo/mcp stretch/curren>
-    printf "(must be all on one line, without the >> << of course) \n "
-    printf "as the script has not been successful it must be done manually \n\n"
+sudo_check(){
+  if [ "$(id -u)" -ne 0 ]; then
+    printf "\n\t$0 must be run as root or sudo\n\n"
+    usage && exit 3
+  else
+    sudook=1
   fi
+}
 
-fi
+getkeys()
+{
+  url=http://downloads.linux.hpe.com/SDR/
 
-sudo apt update
-sudo apt install ssacli -y
+  for key in hpPublicKey1024.pub hpPublicKey2048.pub hpPublicKey2048_key1.pub hpePublicKey2048_key1.pub ; do
+    wget -q -O - "$url$key" |
+    sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/hp-mcp.gpg --import -
+  done
+
+  sudo chmod 644 /etc/apt/trusted.gpg.d/hp-mcp.gpg
+}
+
+sourcelist()   
+{
+  #some systems sudo cannot write the file automated
+  #if we get permission denied have to write it with nano
+  if [ ! -f /etc/apt/sources.list.d/hp-mcp.list ]; then
+
+  addrepo=$(sudo echo "deb [ signed-by=/etc/apt/trusted.gpg.d/hp-mcp.gpg ] http://downloads.linux.hpe.com/SDR/repo/mcp stretch/>
+
+    if [ $(echo "$addrepo" | grep -i denied) ]; then
+      sudo touch /etc/apt/sources.list.d/hp-mcp.list
+      printf "\nPlease do: \n sudo nano /etc/apt/sources.list.d/hp-mcp.list and paste the line \n"
+      printf " >> deb [signed by=/etc/apt/trusted.gpg.d/hp-mcp.gpg ] http://downloads.linux.hpe.com/SDR/repo/mcp stretch/curren>
+      printf "(must be all on one line, without the >> << of course) \n "
+      printf "as the script has not been successful it must be done manually \n\n"
+    fi
+
+  fi
+}
+
+update()
+{
+  sudo apt update
+  sudo apt install ssacli -y
+}
+####### Main #########
+sudocheck
+
+getkeys 
+sourcelist $@
+update 
+
+
